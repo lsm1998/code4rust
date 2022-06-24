7596cf0e3e771b7722653e5993135026bce8168e
shiming.liu <shiming.liu@ly.com>
2022-06-24 14:50:54 +0800

    method

--- src/base/method.rs
+++ src/base/method.rs
@@ -1,3 +1,24 @@
 pub fn method_demo() {
+    let r = Rectangle { width: 1, height: 1 };
+    println!("area={}",r.area());
 
+    let r = Rectangle::new(5,5);
+    println!("area={}",r.area());
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    // 关联函数，类似于构造函数
+    fn new(w: u32, h: u32) -> Rectangle {
+        Rectangle { width: w, height: h }
+    }
+
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
 }
